710eb3e8d7ffead9915ce5d52a347f88afc790f9
Ahmed Riza <dr.riza@gmail.com>
2021-04-16 22:09:42 +0100

    init.el changes

--- init.el
+++ init.el
@@ -130,6 +130,16 @@
 
 (use-package flycheck)
 (use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status)
   :config
   ;; (setq rustic-format-on-save t)
   ;; (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)
@@ -146,6 +156,9 @@
   ;; what to use when checking on-save. "check" is default, I prefer clippy
   (lsp-rust-analyzer-cargo-watch-command "clippy")
   ;; hide sideline code actions
+  (lsp-ui-imenu-enable t)
+  (lsp-ui-imenu-auto-refresh t)
+  (lsp-ui-imenu-window-width 200)
   (lsp-ui-sideline-show-code-actions nil)
   ;; (lsp-eldoc-enable-hover nil)
   (lsp-signature-render-documentation nil)
