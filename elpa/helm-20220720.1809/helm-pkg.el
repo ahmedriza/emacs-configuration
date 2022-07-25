613b6482fa99edf2a219caad3c1df4cab256486e
Ahmed Riza <dr.riza@gmail.com>
2022-07-25 11:15:10 +0100

    disable dap mode so as not to show Run Tests|Debug

---
+++ elpa/helm-20220720.1809/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20220720.1809" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.4")
+    (popup "0.5.3"))
+  :commit "4a3897a2f756c20d460e969427cc6401e1e637d9" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-core-20220722.424/helm-core-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-core" "20220722.424" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "4a3897a2f756c20d460e969427cc6401e1e637d9" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
