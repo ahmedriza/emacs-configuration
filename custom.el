eb13d04221d735b86701725cc4a5cf5ebca34dec
Ahmed Riza <dr.riza@gmail.com>
2022-08-31 16:51:39 +0100

    emacs-configuration

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+ (if (display-graphic-p)
+   '(custom-enabled-themes '(deeper-blue))
+   '(custom-enabled-themes '(manoj-dark)))
+
+ ;; '(custom-enabled-themes '(manoj-dark))
+
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   '(terraform-mode leetcode find-file-in-project hl-todo markdown-mode+ ox-hugo buffer-move selectrum magit toml-mode company helm-lsp lsp-ui rustic xclip dap-mode posframe yasnippet flycheck which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
