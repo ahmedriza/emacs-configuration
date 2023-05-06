dea42fecbf3196dc5c26a47a512ac70795d00f8a
Ahmed Riza <dr.riza@gmail.com>
2023-05-06 22:42:15 +0100

    emacs-configuration

--- init.el
+++ init.el
@@ -555,6 +555,10 @@ Return a list of installed packages or nil for every skipped package."
 ;; use C-c C-o to set offset
 ;; use C-c C-s to show syntactic information (show the variable that needs to be set)
 (add-hook 'java-mode-hook (lambda ()
+                            (setq c-default-style "java")
+                            (c-set-offset 'arglist-intro '+)
+                            (c-set-offset 'arglist-close '0)
+                            (c-set-offset 'case-label '+)
                             (display-line-numbers-mode 1)
                             ))
 
